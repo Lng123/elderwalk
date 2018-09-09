ba3db1081e21d89f9594dff0b975c89ec3379cc6
Kevin Vizconde <kevin.vizconde@gmail.com>
2018-09-08 20:01:42 -0700

    update on jscript

--- js/simulation.js
+++ js/simulation.js
@@ -1,14 +1,29 @@
 var senior = new Image();
 var youngPerson = new Image();
 
-function init() {
+window.onload = function simulate() {
+  
+  console.log("does it start?");
   senior.src = '/Users/user/elderWalk/elderwalk/images/senior.png';
   youngPerson.src = '/Users/user/elderWalk/elderwalk/images/youngPerson.png';
   window.requestAnimationFrame(draw);
 }
 
 function draw() {
-  
+  var ctx = document.getElementById('canvas').getContext('2d');
+  ctx.clearRect(0, 0, 300, 300); // clear canvas
+
+
+
+  //old person
+  ctx.drawImage(senior, 0, 0, 300, 300);
+
+  window.requestAnimationFrame(draw);
+
 }
+
+simulate();
+
+
 
 
