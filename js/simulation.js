1ce96e9c2fc7f0feb460985236bb3ef641bdd394
Louis Ng <louisng1@hotmail.com>
2018-09-08 22:00:20 -0700

    console logs

--- js/simulation.js
+++ js/simulation.js
@@ -16,13 +16,19 @@ function init() {
 init();
 
 
-
-
 var a = 20;
 var b = 20;
 var x = 20;
 var y = 20;
 var z = 20;
+
+var first1 = false;
+var first2 = false;
+var first3 = false;
+var center1 = false;
+var center2 = false;
+var last = false;
+
 function animate() {
   var ctx = document.getElementById('canvas').getContext('2d');
     window.requestAnimationFrame(animate);
@@ -38,6 +44,35 @@ function animate() {
     z += 0.080;
     a += 0.31;
     b += 0.26;
+    
+    if (x > 25 && first1 == false) {
+        console.log("initiated walk, senior detected");
+        first1 = true;
+    }
+
+    if (y > 25 && first2 == false) {
+        console.log("initiated walk, senior detected");
+        first2 = true;
+    }
+
+    if (z > 25 && first3 == false) {
+        console.log("initiated walk, senior detected");
+        first3 = true;
+    }
+
+    
+    if (x > 100 && y > 100 && z > 100 && center2 == false) {
+        console.log("all seniors passed");
+        center2 = true;
+    }
+    
+    if ((x > 200 || y > 200 || z > 200) && last == false) {
+        console.log("street crossed");
+        last = true;
+    }
+    
+    
+    
     
 }
 
